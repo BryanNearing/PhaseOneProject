e4efbe2894de79954e624322a8519e4597137695
BryanNearing <bryannearing@gmail.com>
2020-12-14 13:09:57 -0500

    adding in some last second changes and error handling

--- src/phasePack/Phase1Project.java
+++ src/phasePack/Phase1Project.java
@@ -11,23 +11,58 @@ public class Phase1Project {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
 		
 		System.out.println("******************************************************");
-		System.out.println("WELCOME");
+		System.out.println("WELCOME to Lock.me");
 		System.out.println("This project is the Phase End Project for Phase 1");
 		System.out.println("This project was developed by Bryan Nearing");
 		System.out.println("******************************************************");
-		menu();
+		File dirPath = null;
+		menu(s, dirPath);
 
 	}
 	
-	public static void menu() throws IOException {
+	public static void menu(Scanner s, File dirPath) throws IOException {
+		
+		boolean exists = false;
+		
+		
+		if(dirPath == null)
+			dirPath = new File(System.getProperty("user.dir"));
+		
+		System.out.println("The current directory you are in is " + dirPath.getAbsolutePath() + "\nWould you like to change that? (y/n)");
+		String userInput = s.nextLine();
+		
+		if(userInput.equals("y")) {
+			while(exists == false) {
+				System.out.println("Please list the directory you would like to enter.");
+				String filePath = s.nextLine();
+				
+				dirPath = new File(filePath);
+				
+				if(dirPath.exists()) {
+					System.out.println("Thank you, you are now in the directory at " + filePath);
+					exists = true;
+				}
+				else {
+					System.out.println("That directory does not exist");
+				}
+			
+			}
+		}
+		else if(userInput.equals("n")) {
+			System.out.println("Thank you, you are working in the " + dirPath.getAbsolutePath() + " directory");
+		}
+		else {
+			System.out.println("That was not one of the options.");
+			menu(s, dirPath);
+		}
+		
 		System.out.println("Pick from one of the options");
 		System.out.println("1) Return the file names in ascending order");
 		System.out.println("2) User options");
 		System.out.println("3) Exit the program");
-		
-		Scanner s = new Scanner(System.in);
 		
 		int choice = 0;
 		
@@ -38,28 +73,26 @@ public class Phase1Project {
 		catch(InputMismatchException e) {
 			System.out.println("The option you chose was not a number");
 			System.out.println();
-			menu();
+			menu(s, dirPath);
 		}
 		
 		switch(choice) {
 		case 1:
-			listFiles();
+			listFiles(s, dirPath);
 			break;
 		case 2:
-			userOptionsMenu(s);
+			userOptionsMenu(s, dirPath);
 			break;
 		case 3:
 			break;
 		default:
 			System.out.println("That option was not on the menu, select again.");
-			menu();
+			menu(s, dirPath);
 			break;
 		}
 	}
 	
-	public static void listFiles() throws IOException {
-		
-		 File directoryPath = new File(System.getProperty("user.dir"));
+	public static void listFiles(Scanner s, File directoryPath) throws IOException {
 		 
 		 String contents[] = directoryPath.list();
 		 
@@ -70,10 +103,10 @@ public class Phase1Project {
 		 }
 		 System.out.println();
 		
-		menu();
+		menu(s, directoryPath);
 	}
 	
-	public static void userOptionsMenu(Scanner s) throws IOException {
+	public static void userOptionsMenu(Scanner s, File dirPath) throws IOException {
 		System.out.println("Pick from one of the options");
 		System.out.println("1) Add a file");
 		System.out.println("2) Delete a file");
@@ -89,21 +122,21 @@ public class Phase1Project {
 		catch(InputMismatchException e) {
 			System.out.println("The option you chose was not a number, please pick a number");
 			System.out.println();
-			userOptionsMenu(s);
+			userOptionsMenu(s, dirPath);
 		}
 		
 		switch(choice) {
 		case 1:
-			addFile(s);
+			addFile(s, dirPath);
 			break;
 		case 2:
-			deleteFile(s);
+			deleteFile(s, dirPath);
 			break;
 		case 3:
-			searchFile(s);
+			searchFile(s, dirPath);
 			break;
 		case 4:
-			menu();
+			menu(s, dirPath);
 			break;
 		default:
 			System.out.println("The number chosen was not on the menu, select again.");
@@ -111,26 +144,40 @@ public class Phase1Project {
 		}
 	}
 	
-	public static void addFile(Scanner s) throws IOException {
+	public static void addFile(Scanner s, File dirPath) throws IOException {
 		
 		System.out.println("What is the name of the file you want to add?");
 		String filename = s.nextLine();
 		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(filename));){
+		if(filename.contains("/"))
+			filename = filename.replace("/", "");
+		
+		String filePath = dirPath.getAbsolutePath();
+		
+		String newFilepath = filePath + "/" + filename;
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(newFilepath));){
 			
 		}
 		
 		System.out.println();
 		
-		userOptionsMenu(s);
+		userOptionsMenu(s, dirPath);
 	}
 	
-	public static void deleteFile(Scanner s) throws IOException {
+	public static void deleteFile(Scanner s, File dirPath) throws IOException {
 		System.out.println("What is the name of the file you want to delete?");
 		String filename = s.nextLine();
 		
+		if(filename.contains("/"))
+			filename = filename.replace("/", "");
+		
+		String filePath = dirPath.getAbsolutePath();
+		
+		String newFilepath = filePath + "/" + filename;
+		
 		try{
-			File file = new File(filename);
+			File file = new File(newFilepath);
 			file.delete();
 		}
 		catch(Exception e) {
@@ -139,15 +186,18 @@ public class Phase1Project {
 		
 		System.out.println();
 		
-		userOptionsMenu(s);
+		userOptionsMenu(s, dirPath);
 	}
 	
-	public static void searchFile(Scanner s) throws IOException {
+	public static void searchFile(Scanner s, File dirPath) throws IOException {
 		
 		System.out.println("What is the name of the file you would like to look for?");
 		String filename = s.nextLine();
 		
-		 File directoryPath = new File(System.getProperty("user.dir"));
+		if(filename.contains("/"))
+			filename = filename.replace("/", "");
+		
+		 File directoryPath = dirPath;
 		 
 		 String contents[] = directoryPath.list();
 		 
@@ -165,7 +215,7 @@ public class Phase1Project {
 		 
 		 System.out.println();
 		
-		userOptionsMenu(s);
+		userOptionsMenu(s, dirPath);
 	}
 
 }
